9483f6e8dc296fb06d98c04d16b721b88e373bf0
vikaschoudhary16 <choudharyvikas16@gmail.com>
2017-08-30 10:39:31 -0400

    SFC Device Plugin Baremetal

---
+++ device_plugins/sfc_nic/sfc-nic-move.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+#containerName=${1}
+#NIC=${2}
+containerName="k8s_demo1_my.pod2_default"
+NIC="p2p1"
+rm -f /var/run/netns/$containerName
+containerID=`docker ps | grep $containerName | awk {'print $1'}`
+echo $containerID
+while [ -z $containerID ]; do
+	echo "sleep"
+	containerID=`docker ps | grep $containerName | awk {'print $1'}`
+	  sleep 0.1
+done
+PID=`docker inspect --format '{{ .State.Pid }}' $containerID`
+ln -s /proc/$PID/ns/net /var/run/netns/$containerName
+ip link set dev $NIC netns $containerName
+
